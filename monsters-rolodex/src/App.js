05c9862bf396dff337fe56ab88fd83551e407697
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-12-10 21:57:20 +0000

    Pulled data from API and injected into HTML file in App.js

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            // Set the initial state to an empty array
+            // We want to fill this with the API data
+            monsters: []
+        };
+    }
+    
+    // Use the componentDidMount Life Cycle Method to
+    // fetch the data from the API URL
+    // then convert the response to json so JS can use it
+    // then setState of the initial monsters array to the
+    // body of the API data (response we converted to json)
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ monsters: users }));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {
+                    // Map returns the return of the function passed
+                    // to it, iterated over every element in it
+                    // We create a monster function and return h1
+                    // with monster names
+                    // Pass the ID in as a key on the relevant element
+                    this.state.monsters.map(monster => (
+                    <h1 key={monster.id}> { monster.name } </h1>
+                    ))
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
