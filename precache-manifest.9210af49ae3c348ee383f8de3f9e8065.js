344b2cdb71af07c4953be7580bb1af7906ba985f
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-12-17 21:04:26 +0000

    Updates

---
+++ precache-manifest.9210af49ae3c348ee383f8de3f9e8065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f77ce1f674304e9c94b89f5171f94a1",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "39609f3e681e894fc186",
+    "url": "/react-app/static/css/main.988110b9.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-app/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-app/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "39609f3e681e894fc186",
+    "url": "/react-app/static/js/main.23a00b28.chunk.js"
+  },
+  {
+    "revision": "406138c1ef14348dd013",
+    "url": "/react-app/static/js/runtime-main.fce6c296.js"
+  }
+]);
